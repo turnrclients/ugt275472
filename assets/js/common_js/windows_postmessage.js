45df452a9eca110ba57f5542a77c4bfcd261a121
turnrclients <turnr.clients@outlook.com>
2026-03-02 19:04:36 +0530

    Update windows_postmessage.js

--- assets/js/common_js/windows_postmessage.js
+++ assets/js/common_js/windows_postmessage.js
@@ -6,6 +6,12 @@ window.addEventListener("message", (event) => {
         localStorage.setItem("owner", event.data.owner);
         localStorage.setItem("repo_name", event.data.repo_name);
         document.getElementById("output").textContent = "Received message: " + JSON.stringify(event.data);
+        if (event.data.message !== "false") {
+        // This ensures the DOM is fully ready
+        setTimeout(() => createButtons(), 0);
+    } else {
+        showCustomAlertBox('error', 'Feature is disabled');
+    }
         event.source.postMessage({ reply: "Got your message!" }, event.origin);
 
       });
